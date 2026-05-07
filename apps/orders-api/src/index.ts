7dabc817e312764a5f6fa2bc9e0b51676a13b493
andrestorresgo <andytorresgonz@gmail.com>
2026-05-07 10:47:16 -0400

    feat: installed and initialized drizzle

--- apps/orders-api/src/index.ts
+++ apps/orders-api/src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { usersTable } from "./db/schema";
 
+const db = drizzle(process.env.DATABASE_URL!);
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello from Orders API!");
+app.get("/", async (c) => {
+  return c.json({
+    data: await db.select().from(usersTable),
+  });
 });
 
 app.get("/health", (c) => {
